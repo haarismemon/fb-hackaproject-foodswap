7e57d0235cfee65b684b4bf251386e38b74090f7
Haaris Memon <memonhaaris97@gmail.com>
2018-10-17 11:20:42 +0100

    Create a data model pojo class for a user

---
+++ app/src/main/java/com/hackaproject/foodswap/foodswap/DataModel/User.java
@@ -0,0 +1,77 @@
+package com.hackaproject.foodswap.foodswap.DataModel;
+
+public class User {
+
+    private String first_name;
+    private String last_name;
+    private String gender;
+    private String email;
+    private String password;
+    private String nationality;
+    private String dietary;
+    private String dob;
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getDietary() {
+        return dietary;
+    }
+
+    public void setDietary(String dietary) {
+        this.dietary = dietary;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+}
