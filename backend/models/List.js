e1ed930bd02d41069064e3a4c7f7aef68e8a4c96
jojojolin <jolinechien@yahoo.com.tw>
2018-10-30 02:57:02 +0000

    Added model/table List

--- backend/models/List.js
+++ backend/models/List.js
@@ -5,11 +5,14 @@ const List = sequelize.define(
     'List', {
         //id: {type: Sequelize.INTEGER, autoIncrement:true},
         uid: {type:Sequelize.INTEGER, allowNull: false},
+        //the food that user wants to cook
         food:{type:Sequelize.STRING(50), allowNull: false},
         //0-pending, 1-confirm, 2-done
         status:{type: Sequelize.INTEGER, allowNull: false},
         //'YYYY-MM-DD'
-        date:{type:Sequelize.DATE, allowNull: false}
+        date:{type:Sequelize.DATE, allowNull: false},
+        partnerid: {type:Sequelize.INTEGER, allowNull: true}
+        
     },{
         freezeTableName: true
     }
