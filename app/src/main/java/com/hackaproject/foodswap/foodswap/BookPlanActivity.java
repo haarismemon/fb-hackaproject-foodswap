b38abafe9d05a3fd1da0eea2ef69cf386ac67751
Haaris Memon <memonhaaris97@gmail.com>
2018-11-06 22:38:32 +0000

    Add ability to make request to book a plan

--- app/src/main/java/com/hackaproject/foodswap/foodswap/BookPlanActivity.java
+++ app/src/main/java/com/hackaproject/foodswap/foodswap/BookPlanActivity.java
@@ -1,13 +1,82 @@
 package com.hackaproject.foodswap.foodswap;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+import com.hackaproject.foodswap.foodswap.datamodels.NewEvent;
+import com.hackaproject.foodswap.foodswap.requests.BookingRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class BookPlanActivity extends AppCompatActivity {
+
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_plan);
+
+        sharedPreferences = getApplicationContext().getSharedPreferences("DATA", Context.MODE_PRIVATE);
+    }
+
+    public void bookEvent(View view) {
+        EditText foodText = findViewById(R.id.input_food);
+        EditText dateText = findViewById(R.id.input_date);
+
+        String loggedInUID = sharedPreferences.getString(HomeActivity.LOGGED_IN_UID, null);
+
+        NewEvent newEvent = new NewEvent(loggedInUID, foodText.getText().toString(), dateText.getText().toString());
+
+        if(loggedInUID != null) {
+            BookingRequest bookingRequest = new BookingRequest(newEvent, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        JSONObject jsonResponse = new JSONObject(response);
+                        int responseStatus = jsonResponse.getInt("status");
+
+                        if (responseStatus == 1) {
+                            Toast.makeText(BookPlanActivity.this, "Event created!", Toast.LENGTH_SHORT).show();
+
+                            Intent intent = new Intent(BookPlanActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                        } else {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(BookPlanActivity.this);
+                            builder.setMessage("Event Creation Failed")
+                                    .setNegativeButton("Retry", null)
+                                    .create()
+                                    .show();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("FoodSwap", "Book Plan. Error Message. Failed Response" + error);
+                }
+            });
+
+            RequestQueue queue = Volley.newRequestQueue(BookPlanActivity.this);
+            queue.add(bookingRequest);
+            queue.start();
+        } else {
+            Toast.makeText(this, "uid is null", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/hackaproject/foodswap/foodswap/HomeActivity.java
+++ app/src/main/java/com/hackaproject/foodswap/foodswap/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.hackaproject.foodswap.foodswap;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -9,19 +10,30 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.Volley;
+import com.hackaproject.foodswap.foodswap.datamodels.Event;
+import com.hackaproject.foodswap.foodswap.requests.ListRequest;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeActivity extends AppCompatActivity {
 
-    public static final String LOGGED_IN = "LOGGED_IN";
+    public static final String LOGGED_IN_EMAIL = "LOGGED_IN_EMAIL";
+    public static final String LOGGED_IN_UID = "LOGGED_IN_UID";
+    private String loggedInEmail;
+    private String loggedInUID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,49 +41,75 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("DATA", Context.MODE_PRIVATE);
-//        sharedPreferences.edit().putBoolean(LOGGED_IN, true).apply();
-        boolean isLoggedIn = sharedPreferences.getBoolean(LOGGED_IN, false);
+//        sharedPreferences.edit().putString(LOGGED_IN_EMAIL, null).apply();
+//        sharedPreferences.edit().putString(LOGGED_IN_UID, null).apply();
+        loggedInEmail = sharedPreferences.getString(LOGGED_IN_EMAIL, null);
+        loggedInUID = sharedPreferences.getString(LOGGED_IN_UID, null);
 
-        if(!isLoggedIn) {
+        if(loggedInEmail == null || loggedInUID == null) {
             Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
+        } else {
+
+            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(HomeActivity.this, BookPlanActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+            //make a list request on startup
+            updateEventList();
         }
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(HomeActivity.this, BookPlanActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        ListRequest listRequest = new ListRequest(new Response.Listener<String>() {
+    private void updateEventList() {
+        ListRequest listRequest = new ListRequest(loggedInUID, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 try {
                     Toast.makeText(HomeActivity.this, "Response Received", Toast.LENGTH_SHORT).show();
                     JSONObject jsonResponse = new JSONObject(response);
-                    boolean success = jsonResponse.getBoolean("success");
+                    int responseStatus = jsonResponse.getInt("status");
 
-                    if (success) {
-//                        todo get list of plans
+                    if (responseStatus == 1) {
+                        List<Event> events = new ArrayList<>();
+
+                        JSONArray jsonArray = jsonResponse.getJSONArray("list");
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                            Event event = new Event();
+                            event.setUid(jsonObject.getString("uid"));
+                            event.setFood(jsonObject.getString("food"));
+                            event.setDate(jsonObject.getString("date"));
+                            event.setStatus(jsonObject.getString("status"));
+                            event.setPartnerId(jsonObject.getString("partnerid"));
+
+                            events.add(event);
+                        }
+
+                        ArrayAdapter<String> adapter = new ArrayAdapter<String>(HomeActivity.this,
+                                android.R.layout.simple_list_item_1, android.R.id.text1, getEventStrings(events));
+
+                        ListView listView = findViewById(R.id.eventsListView);
+                        listView.setAdapter(adapter);
                     } else {
-                        AlertDialog.Builder builder = new AlertDialog.Builder(HomeActivity.this);
-                        builder.setMessage("List Get Failed")
-                                .setNegativeButton("Retry", null)
-                                .create()
-                                .show();
+                        showErrorDialog("List Retrieval Failed. Response code: " + responseStatus);
                     }
-                } catch (JSONException e) {
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.d("FoodSwap", "Error Message. Failed Response" + error);
+                Log.d("FoodSwap", "HomeActivity. Error Message. Failed Response" + error);
+                showErrorDialog("List Retrieval Failed.");
             }
         });
 
@@ -80,8 +118,41 @@ public class HomeActivity extends AppCompatActivity {
         queue.start();
     }
 
+    private void showErrorDialog(String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(HomeActivity.this);
+        builder.setMessage(message)
+                .setNegativeButton("Retry", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        updateEventList();
+                    }
+                })
+                .create()
+                .show();
+    }
+
     public void bookPlan(View view) {
         Intent intent = new Intent(HomeActivity.this, BookPlanActivity.class);
         this.startActivity(intent);
+    }
+
+    public List<String> getEventStrings(List<Event> events) {
+        List<String> strings = new ArrayList<>();
+
+        for(Event event : events) {
+            String title = event.getFood() + " - ";
+
+            if(event.getStatusInteger() == 0) {
+                title += "pending";
+            } else if(event.getStatusInteger() == 1) {
+                title += "confirmed";
+            } else if(event.getStatusInteger() == 2) {
+                title += "done";
+            }
+
+            strings.add(title);
+        }
+
+        return strings;
     }
 }
